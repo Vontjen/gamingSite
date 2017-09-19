6d66712120110b9ed218a92d37c25fa48876c152
Vontjen <vontjen@gmail.com>
2017-09-19 10:56:59 +0200

    initial resources

---
+++ src/main/java/com/vdab/gamingSite/GamingSiteApplication.java
@@ -0,0 +1,12 @@
+package com.vdab.gamingSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingSiteApplication.class, args);
+	}
+}
